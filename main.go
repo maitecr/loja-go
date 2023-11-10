7e5f6b0f0b692b997df1ef721f384eccbc09043d
Maite Capistrano Refosco <maite.refosco@eldorado.org.br>
2023-11-10 08:48:01 -0300

    cont

--- main.go
+++ main.go
@@ -5,13 +5,36 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html")) //encapsula os templates e devolve 2 retornos (template e msg de erro)
 
 func main() {
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", Index)
 	http.ListenAndServe(":8000", nil)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	temp.Execute(w, "index")
+func Index(w http.ResponseWriter, r *http.Request) {
+
+	produtos := []Produto{
+		{
+			Nome:       "Camiseta",
+			Descricao:  "Preta",
+			Preco:      20,
+			Quantidade: 3,
+		},
+		{
+			"Tênis",
+			"Azul",
+			40,
+			9,
+		},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
