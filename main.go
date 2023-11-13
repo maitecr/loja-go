f4d66c904fe069d8235b92f9c27167b690f1d917
Maite Capistrano Refosco <maite.refosco@eldorado.org.br>
2023-11-13 10:20:15 -0300

    conexao bd e retorno html

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
+//go mod init
+//go mod tidy
+//go build
+
+func conectarBD() *sql.DB {
+	conexao := "user=postgres dbname=loja password=postgres host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -21,20 +40,38 @@ func main() {
 
 func Index(w http.ResponseWriter, r *http.Request) {
 
-	produtos := []Produto{
-		{
-			Nome:       "Camiseta",
-			Descricao:  "Preta",
-			Preco:      20,
-			Quantidade: 3,
-		},
-		{
-			"Tênis",
-			"Azul",
-			40,
-			9,
-		},
+	db := conectarBD()
+
+	selectProdutos, err := db.Query("select * from produtos")
+
+	if err != nil {
+		panic(err)
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+
 	}
 
 	temp.ExecuteTemplate(w, "Index", produtos)
+
+	defer db.Close()
 }
